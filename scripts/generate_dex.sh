01c31f342dbaad40388a08577a6ae417e320ad1d
Nightmare <mengyanshou@gmail.com>
2021-11-01 19:36:18 +0800

    update server jar

--- scripts/generate_dex.sh
+++ scripts/generate_dex.sh
@@ -5,5 +5,7 @@ $LOCAL_DIR/apktool.sh d $apkPath -r -f -o $LOCAL_DIR/tmp
 rm -rf $LOCAL_DIR/tmp/lib
 rm -rf $LOCAL_DIR/tmp/assets
 rm -rf $LOCAL_DIR/tmp/res
+rm -rf $LOCAL_DIR/tmp/resources.arsc
+rm -rf $LOCAL_DIR/tmp/AndroidManifest.xml
 $LOCAL_DIR/apktool.sh b -f $LOCAL_DIR/tmp -o $LOCAL_DIR/server.jar
 rm -rf $LOCAL_DIR/tmp
